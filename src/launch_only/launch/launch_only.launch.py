8b7851aff7f822aaf9a000efb2a34d1e47590d44
LuZhishan <2372798812@qq.com>
2024-08-10 15:27:44 +0800

    feat:add launch_only
    Signed-off-by: LuZhishan <2372798812@qq.com>

---
+++ src/launch_only/launch/launch_only.launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    share_dir = get_package_share_directory('launch_only')
+    xacro_path = os.path.join(share_dir, 'config', 'robot.urdf.xacro')
+    rviz_config_file = os.path.join(share_dir, 'config', 'rviz2.rviz')
+
+    print("urdf_file_name : {}".format(xacro_path))
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro', ' ', xacro_path])
+            }]
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_transform_publisher',
+            arguments='0 0 1.5 0 0 0 0 base_link rslidar'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_file],
+            output='screen'
+        )
+    ])
